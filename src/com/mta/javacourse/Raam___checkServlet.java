6b432bbc6b7be3bc84917362707eea734eb03b2b
אשתי האחת והיחידה <  @Lenovo-PC>
2014-11-09 17:10:49 +0200

    1

--- src/com/mta/javacourse/Raam___checkServlet.java
+++ src/com/mta/javacourse/Raam___checkServlet.java
@@ -5,7 +5,19 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Raam___checkServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		resp.setContentType("text/html");
+		int num1, num2, num3;
+		
+		num1= 3;
+		num2= 4;
+		num3= 7;
+		
+		int result = (num1+num2)*num3;
+		
+		String resultStr=new String("Result of " + "(" + num1 + "+" + num2 + ")"+"*"+num3 +"="+ result) ;
+		
+				
+		resp.getWriter().println(resultStr);
 	}
 }
